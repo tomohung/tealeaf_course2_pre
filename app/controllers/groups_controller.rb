beab001f0418fc9acf06048863e80ef177ca9823
Tomo Hung <tomohung@gmail.com>
2015-01-26 21:43:38 +0800

    add group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,20 +5,44 @@ class GroupsController < ApplicationController
   end
 
   def create
-    @group = Group.new(user_params)
+    @group = Group.new(group_params)
 
     respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
+      if @group.save
+        format.html { redirect_to @group, notice: 'Group was successfully created.' }
+        format.json { render :show, status: :created, location: @group }
       else
         format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        format.json { render json: @group.errors, status: :unprocessable_entity }
       end
     end
   end
 
+  def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+    respond_to do |format|
+      format.html { redirect_to groups_url, notice: 'Group was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end  
+
   def show
     @group = Group.find(params[:id])
+    @users = User.all.where(:group == @group.name)
   end
+
+  def index
+    @groups = Group.all
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+private
+    def group_params
+      params.require(:group).permit(:name)
+    end
+
 end
